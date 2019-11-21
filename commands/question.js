a8433dbbb1ef7ec48db18b1c8be4e6889c806a02
FDSoftware <dfrenoux@gmail.com>
2019-11-21 00:42:53 -0300

    test

--- commands/question.js
+++ commands/question.js
@@ -13,7 +13,7 @@ module.exports = {
       //sacamos el primero
       var test3 = test2.shift();
       console.log(test3);
-      message.channel.send(`<@!${test3[0]['User']}>  Pregunto: **${test3[0]['QA']}** `);
+      message.channel.send(`<${test3[0]['User']}>  Pregunto: **${test3[0]['QA']}** `);
       //re-parseamos el json para guardar
       let data2 = JSON.stringify(test2, null, 2);
       fs.writeFileSync('QA.json', data2);
@@ -46,9 +46,8 @@ module.exports = {
       //escribimos:
       let data2 = JSON.stringify(QAContruct, null, 2);
       fs.writeFileSync('QA.json', data2);
-     
-
       return  message.channel.send(`✅ Tu pregunta ya se envio! `);
+
     } else if(args[1] == "start"){
       /* para iniciar el archivo*/
       var QA3 = '[[ {  "User": "test2", "QA": "manda otro shift, esto es solo para iniciar el array!!" } ]]';
@@ -56,16 +55,22 @@ module.exports = {
      // let data3 = JSON.stringify(QA3, null, 2);
       fs.writeFileSync('QA.json', QA3);
       return message.channel.send(" ✅ Modulo Iniciado");
+
     }else if (args[1] == "end"){
+
       fs.writeFileSync('QA.json', "");
       message.channel.send("listo");
+
     }else if(args[1] == "history") {
       //Primero leemos:
       let rawdata = fs.readFileSync('QA.history.json');
       let QAContruct4 = JSON.parse(rawdata);
       //escribimos:
       message.channel.send(`✅ Historial de pregunas:`);
-      return  message.channel.send(`${QAContruct4}`);
+      QAContruct4.forEach(element => {
+       console.log(` <${element.User}> pregunto:  ${element.QA} `);
+      });
+      return;
      }else {
        return message.channel.send(`Para usar el modulo de QA usa el siguiente comando:
       \n !question push "tu pregunta aca" (no hace faltan las comillas , si respetar los espacios)`);
